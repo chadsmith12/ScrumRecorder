4f44c292784cce421d454980ef5afdf88f84eccc
Chad Smith <smith.chad12@gmail.com>
2022-02-22 21:42:04 -0600

    Initial Commit

---
+++ ScrumRecorder/ScrumRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumRecorderApp.swift
+//  ScrumRecorder
+//
+//  Created by Chad Smith on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
